733b1f03bac60e4c80ab08b0d80ba787615c56ae
Lovera Gabriel <gabrielloveraa@gmail.com>
2021-09-17 15:00:26 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  const appTittle = "To-Do App";
+  return (
+    <div className="ui container center aligned">
+      <h1>{appTittle}</h1>
+    </div>
+  );
+}
+
+export default App;
